9ab34867836a5f4460d6db5ecfa23580229a7312
root <root@DESKTOP-3S5SIJL.localdomain>
2023-12-21 20:30:14 +0300

    submitted

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+//  Move the dodger to left
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+// Move the dodger to right
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10) || 0;
+
+  if (left < window.innerWidth - dodger.clientWidth) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
